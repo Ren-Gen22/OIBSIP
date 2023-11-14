251b520e2aebfd91ef9e24356e381ed723aecae1
Ren-Gen22 <abhishekrenjan33@gmail.com>
2023-11-14 14:12:27 +0530

    changes

--- proj3_weather/Init.py
+++ proj3_weather/Init.py
@@ -2,30 +2,49 @@ import fetch_weather as fw
 from tkinter import *
 import os
 from PIL import Image, ImageTk
-import requests
-from io import BytesIO
+def current_dir(a,b="",c=""):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    if b!="":
+        icon_path = os.path.join(script_dir, a,b,c)
+    else:
+        icon_path = os.path.join(script_dir, a)    
+    icon_path=icon_path.replace("\\", "/")
+    return icon_path
 
 def disp_weather():
     location=e1.get()
     unit=v.get()
     url=fw.get_icon()
-    new_window = Toplevel(root)
-    new_window.geometry("300x200")
-    new_window.title("New Window")
+    conditon=fw.get_conditon()
+    Temp,feelTemp=fw.get_temperature(unit)
     split_url=url.split("/")
-    time=split_url[len(split_url)-2]
+    time=split_url[len(split_url)-2] 
     logo=split_url[len(split_url)-1]
-    
+    wImg=current_dir("weatherImg",time,logo)
+    #print(wImg)
+
+    image_window = Toplevel(root)
+    image_window.geometry("300x300")
+    img = Image.open(wImg)
+    img = ImageTk.PhotoImage(img)
+    image = Label(image_window, image=img)
+    image.image = img  
+    image.pack(side=TOP)
+
+    Label(image_window,text=conditon).pack(side=TOP)
+    Label(image_window,text=Temp).pack(side=TOP)
+    Label(image_window,text=feelTemp).pack(side=TOP)
 
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-icon_path = os.path.join(script_dir, "weather/weather.ico")
-icon_path=icon_path.replace("\\", "/")
 
+
+
+
+icon_path=current_dir("weatherImg/weather.ico")
 root = Tk()
 root.title("Weather")
 root.iconbitmap(default=icon_path)
-root.geometry("400x300")
+root.geometry("400x100")
 
 Label(root, text='Enter the Location:').grid(row=0, column=0, pady=4, sticky=W)
 
@@ -34,8 +53,8 @@ e1.grid(row=0, column=1, pady=5, sticky=W)
 
 v = IntVar()
 Label(root,text='Choose a unit:').grid(row=1,column=0,pady=5,sticky=W)
-Radiobutton(root, text='Celsius', variable=v, value=1).grid(row=1, column=1, pady=4, sticky=W)
-Radiobutton(root, text='Fahrenheit', variable=v, value=2).grid(row=1, column=2, pady=4, sticky=W)
+Radiobutton(root, text='Celsius', variable=v, value=0).grid(row=1, column=1, pady=4, sticky=W)
+Radiobutton(root, text='Fahrenheit', variable=v, value=1).grid(row=1, column=2, pady=4, sticky=W)
 
 fetch_button = Button(root, text="Find", command=disp_weather)
 fetch_button.grid(row=3, column=0, pady=10, columnspan=3)
--- proj3_weather/fetch_weather.py
+++ proj3_weather/fetch_weather.py
@@ -3,23 +3,35 @@ import json
 import requests
 from dotenv import load_dotenv
 
-location="Chennai" #input("Enter location:")
-load_dotenv()
-api=os.getenv("API_KEY")
-url=f"http://api.weatherapi.com/v1/current.json?key={api}&q={location}"
-response=requests.get(url)
-if response.status_code == 200:
-    data_dict = response.json()
-else:
-    print(f"Error: {response.status_code}")
+class Weatherify:
+    data_dict = {}
 
-def get_conditon():
-    return data_dict["current"]["condition"]["text"]
-def get_icon():
-    return data_dict["current"]["condition"]["icon"]
-def get_temperature(unit):
-    if unit=="c":
-        return data_dict["current"]["temp_c"],data_dict["current"]["feelslike_c"]
-    elif unit=="f":
-        return data_dict["current"]["temp_f"],data_dict["current"]["feelslike_f"]
+    def __init__(self, location):
+        self.location = location
+        load_dotenv()
+        api = os.getenv("API_KEY")
+        url = f"http://api.weatherapi.com/v1/current.json?key={api}&q={location}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            self.data_dict = response.json()  
+        else:
+            print(f"Error: {response.status_code}")
 
+    def get_condition(self):
+        return self.data_dict["current"]["condition"]["text"]
+
+    def get_icon(self):
+        return self.data_dict["current"]["condition"]["icon"]
+
+    def get_temperature(self, unit):
+        if unit == 0:
+            return self.data_dict["current"]["temp_c"], self.data_dict["current"]["feelslike_c"]
+        elif unit == 1:
+            return self.data_dict["current"]["temp_f"], self.data_dict["current"]["feelslike_f"]
+
+
+# location = "Chennai"
+# weather = Weatherify(location)
+# print(weather.get_condition())
+# print(weather.get_icon())
+# print(weather.get_temperature(0))
